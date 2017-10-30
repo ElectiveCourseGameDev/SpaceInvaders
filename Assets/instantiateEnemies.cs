d12e8fcb208984bc7a07a66f30d135b1e06dc103
Ebbe Vang <ev@evang.dk>
2017-10-30 14:16:04 +0100

    adds project to git

---
+++ Assets/instantiateEnemies.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiateEnemies : MonoBehaviour {
+
+    public int enemyRows = 4;
+    public int enemyRowLength = 16;
+    public float enemyDistance = 1f;
+    public float enemyRowDistance = 1f;
+    public Vector2 startingPoint;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < enemyRows; i++)
+        {
+            for (int j = 0; j < enemyRowLength; j++)
+            {
+                GameObject enemyInstance = Instantiate(Resources.Load("enemy1", typeof(GameObject))) as GameObject;
+                enemyInstance.transform.Translate(startingPoint + new Vector2(enemyDistance*j, enemyRowDistance*i));
+
+
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
