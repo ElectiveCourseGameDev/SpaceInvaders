2dbd0165daccf43999673d24fc7b96b5aed802a0
Ebbe Vang <ev@evang.dk>
2017-10-30 15:12:48 +0100

    adds player and bullet objects

---
+++ Assets/PlayerShooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+
+
+    public float movement;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bullet = Instantiate(Resources.Load("Bullet", typeof(GameObject))) as GameObject;
+            bullet.transform.position = transform.position;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * movement);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * movement);
+        }
+    }
+}
