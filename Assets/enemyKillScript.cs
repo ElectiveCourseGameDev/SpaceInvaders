6450aed4a7b1e26d9c6dd22f267983e6308ad0ba
Ebbe Vang <ev@evang.dk>
2017-11-06 15:41:35 +0100

    adds feature: bullets kills enemies

---
+++ Assets/enemyKillScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyKillScript : MonoBehaviour {
+
+    public void killEnemy()
+    {
+        //Debug.Log("Enemy killed by bullet");
+        GameObject.Destroy(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
